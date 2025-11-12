91fb302a64c020f8baddac478ccda50c590ba4a1
Cristian Tumani <cristiantumani@gmail.com>
2025-11-12 22:08:54 +0100

    Refactor to use 'ul' and 'li' elements
    Change the creation of list elements to use 'ul' and 'li'.

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -2,7 +2,7 @@ import { createOptimizedPicture } from '../../scripts/aem.js';
 
 export default function decorate(block) {
   /* change to ul, li */
-  const ul = document.createElement('ul');
+  const li  = document.createElement('li');
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
     while (row.firstElementChild) li.append(row.firstElementChild);
